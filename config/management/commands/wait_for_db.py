76f5781975a7be23599fdc3637cc5f4700d4bb2b
Alexandr <oleksandrglushen2005@gmail.com>
2026-01-04 18:17:03 +0200

    Add Docker setup and PostgreSQL integration .

---
+++ config/management/commands/wait_for_db.py
@@ -0,0 +1,21 @@
+import time
+from django.core.management.base import BaseCommand
+from django.db import connections
+from django.db.utils import OperationalError
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        self.stdout.write("Waiting for database...")
+        db_conn = None
+
+        while True:
+            try:
+                db_conn = connections["default"]
+                db_conn.cursor()
+                break
+            except OperationalError:
+                self.stdout.write("Database unavailable, waiting...")
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("Database available"))
--- config/settings.py
+++ config/settings.py
@@ -13,17 +13,17 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
-import load_dotenv
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-load_dotenv.load_dotenv()
+load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'apps.borrowing',
     'apps.notifications',
     'apps.payments',
+    'config',
 ]
 
 MIDDLEWARE = [
@@ -85,9 +86,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
@@ -152,3 +157,10 @@ AUTH_USER_MODEL = 'users.User'
 
 CELERY_BROKER_URL = "redis://localhost:6379/0"
 CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
